4d0b6576997ef5ba23926af39f95e1f4cb3f5d09
JP Ferreira <jpferreira@MA301.broadband>
2021-05-10 16:10:22 +0100

    Adding the random cat h1

--- app.rb
+++ app.rb
@@ -13,6 +13,9 @@ get '/football' do
   "Man Utd stopped Man City from winning the Premier League"
 end
 
+
+
 get '/cat' do
-  '<div style="border: 2px dashed red;"><img src="https://i.imgur.com/jFaSxym.png"></div>'
+  @name_arr = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
 end
