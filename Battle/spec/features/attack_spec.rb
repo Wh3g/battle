9dbdb4a242911406451e212ccee55995965d8476
willoughby_hood <willoughbyhood29@googlemail.com>
2021-05-12 18:42:54 +0100

    added hit points functionality

--- Battle/spec/features/attack_spec.rb
+++ Battle/spec/features/attack_spec.rb
@@ -4,4 +4,12 @@ feature "attack" do
     click_button "Attack"
     expect(page).to have_content "John attacked James"
   end
+
+  scenario "reduce player 2 hp by 10" do
+    sign_in_and_play
+    click_button "Attack"
+    click_link "OK"
+    expect(page).not_to have_content "James: 100hp"
+    expect(page).to have_content "James: 90hp"
+  end
 end
--- Battle/spec/player_spec.rb
+++ Battle/spec/player_spec.rb
@@ -1,8 +1,25 @@
 describe Player do
-  subject(:player) {Player.new("John")}
+  subject(:player_1) {Player.new("John")}
+  subject(:player_2) {Player.new("James")}
   describe "#name" do
     it "returns a name" do
-      expect(player.name).to eq "John"
+      expect(player_1.name).to eq "John"
+    end
+  end
+  describe "#hit_points" do
+    it "returns the hit points" do
+      expect(player_1.hit_points).to eq Player::DEFAULT_HIT_POINTS
+    end
+  end
+  describe "#attack" do
+    it "attacks another player" do
+      expect(player_2).to receive(:damage)
+      player_1.attack(player_2)
+    end
+  end
+  describe "#damage" do
+    it "reduces hit points by 10" do
+      expect{ player_1.damage }.to change {player_1.hit_points}.by(-10)
     end
   end
 end
