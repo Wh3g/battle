6eb7d43bc76fc72c0be3202ad9a730f8d3d18be8
willoughby_hood <willoughbyhood29@googlemail.com>
2021-05-13 16:00:03 +0100

    Game now accepts 2 Player classes as arguments, plus refactoring

--- Battle/app.rb
+++ Battle/app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require 'capybara'
 require './lib/player'
+require './lib/player'
 
 
 
@@ -18,21 +19,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1
-    @player_2 = $player_2
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    Game.new.attack(@player_2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb(:attack)
   end
 
--- Battle/spec/game_spec.rb
+++ Battle/spec/game_spec.rb
@@ -1,11 +1,20 @@
 require 'game'
 
 describe Game do
-  let(:player) { double(name: "John", hit_points: Player::DEFAULT_HIT_POINTS)}
+  subject(:game) { described_class.new(player_1, player_2) }
+    let(:player_1) { double :player }
+    let(:player_2) { double :player }
   describe '#attack' do
     it "attacks a player" do
-      expect(player).to receive(:damage)
-      subject.attack(player)
+      expect(player_1).to receive(:damage)
+      subject.attack(player_1)
+    end
+  end
+
+  describe "#player_1 and #player_2" do
+    it "accepts two player instances" do
+      expect(game.player_1).to eq player_1
+      expect(game.player_2).to eq player_2
     end
   end
 end
